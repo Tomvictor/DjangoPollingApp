4244c532ccc3131dca3e44e1657fa5d8b6dc42cc
tomvictor <tom@buildfromzero.com>
2016-01-22 09:44:23 +0530

    initial commit
    virtual env
    and
    the code

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Tomvictor/Documents/Git Repository/Django/PollingApp/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
